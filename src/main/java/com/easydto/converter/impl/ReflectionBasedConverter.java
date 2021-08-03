b3eb89dae1d65f8966f2f3f8566097f952f99452
rahul-acr <rahul.saha.c@gmail.com>
2021-08-03 08:46:17 +0530

    Minor refactoring

--- src/main/java/com/easydto/converter/impl/ReflectionBasedConverter.java
+++ src/main/java/com/easydto/converter/impl/ReflectionBasedConverter.java
@@ -2,6 +2,7 @@ package com.easydto.converter.impl;
 
 import com.easydto.converter.FieldConfiguration;
 import com.easydto.exception.DtoConversionException;
+import com.easydto.exception.DtoException;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -31,7 +32,7 @@ public class ReflectionBasedConverter {
         try {
            return clazz.getConstructor().newInstance();
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-            throw new DtoConversionException(e);
+            throw new DtoException("Failed to invoke no-arg construtor for "+clazz+". Please declare a no-arg constructor", e);
         }
     }
 
--- src/main/java/com/easydto/exception/DtoException.java
+++ src/main/java/com/easydto/exception/DtoException.java
@@ -1,7 +1,12 @@
 package com.easydto.exception;
 
 public class DtoException extends RuntimeException{
+
     DtoException(Exception e){
         super(e);
+    }
+
+    public DtoException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
